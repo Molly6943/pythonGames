28a181e7a34e9bb60f614f19d38f70a15596e1b6
Molly Liu <873454517@qq.com>
2017-03-18 14:15:41 +0800

    change move style

--- mutiplPicMove.py
+++ mutiplPicMove.py
@@ -14,28 +14,39 @@ class myBallClass(pygame.sprite.Sprite):
         if self.rect.top < 0 or self.rect.bottom > height:
             self.speed[1] = - self.speed[1]
 
+def animate(group):
+    screen.fill([255,255,255])
+    for ball in group:
+        ball.move()
+    for ball in group:
+        group.remove(ball)
+        if pygame.sprite.spritecollide(ball,group,False):
+            ball.speed[0] = - ball.speed[0]
+            ball.speed[1] = - ball.speed[1]
+        group.add(ball)
+        screen.blit(ball.image,ball.rect)
+
+    pygame.display.flip()
+    # pygame.time.delay(20)
+
+
 size = width,height = 640,480
 screen = pygame.display.set_mode(size)
 screen.fill([255,255,255])
 img_file = "no.png"
-balls = []
-for row in range(0,3):
-    for column in range(0,3):
+clock = pygame.time.Clock()
+group = pygame.sprite.Group()
+for row in range(0,2):
+    for column in range(0,2):
         location = [column * 180 + 10,row * 180 + 10]
         speed = [choice([-2,2]),choice([-2,2])]
         ball = myBallClass(img_file,location,speed)
-        balls.append(ball)
-for ball in balls:
-    screen.blit(ball.image,ball.rect)
-pygame.display.flip()
-
+        group.add(ball)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            frame_rate = clock.get_fps()
+            print "frame_rate = ",frame_rate
             sys.exit()
-    pygame.time.delay(20)
-    screen.fill([255,255,255])
-    for ball in balls:
-        ball.move()
-        screen.blit(ball.image,ball.rect)
-    pygame.display.flip()
+    animate(group)
+    clock.tick(30)
